b3b54f3a58288381a7cd2913593fa560c4c003c7
s.lagutin <s.lagutin@ts-soft.ru>
2015-11-27 17:27:24 +0300

    Выбор отдельных плагинов для gulp

--- generators/app/index.js
+++ generators/app/index.js
@@ -27,7 +27,7 @@ module.exports = yeoman.generators.Base.extend({
                 'default': false
             },
             {
-                type: 'checkbox',
+                type: 'list',
                 name: 'features',
                 message: 'What more framework would you install?',
                 choices: [{
@@ -48,6 +48,29 @@ module.exports = yeoman.generators.Base.extend({
                     value: 'includeModernizr',
                     checked: false
                 }]
+            },
+            {
+                type: 'checkbox',
+                name: 'plugins',
+                message: 'What plugin would you include for Gulp?',
+                choices: [{
+                    name: 'LESS',
+                    value: 'includeLess',
+                    checked: true
+                }, {
+                    name: 'Karma',
+                    value: 'includeKarma',
+                    checked: true
+                }, {
+                    name: 'JSCS',
+                    value: 'includeJscs',
+                    checked: true
+                },
+                {
+                    name: 'ESLint',
+                    value: 'includeEslint',
+                    checked: true
+                }]
             }
         ];
         this.prompt(prompts, function (answers) {
@@ -61,6 +84,11 @@ module.exports = yeoman.generators.Base.extend({
             this.includeReact = hasFeature('includeReact');
             this.includeBackbone = hasFeature('includeBackbone');
             this.includeModernizr = hasFeature('includeModernizr');
+
+            this.includeLess = hasFeature('includeLess');
+            this.includeKarma = hasFeature('includeKarma');
+            this.includeJscs = hasFeature('includeJscs');
+            this.includeEslint = hasFeature('includeEslint');
 
             if (this.includeTwitterBootStrap) {
                 this.bowerInstall(['twitter'], { 'save': true })
@@ -86,26 +114,34 @@ module.exports = yeoman.generators.Base.extend({
         mkdirp("src/less");
         mkdirp("tests");
         this.copy("_gulpfile.js", "gulpfile.js");
-        this.copy("_eslint.config.json", "eslint.config.json");
         this.copy("_package.json", "package.json");
-        this.copy("_.jscsrc", ".jscsrc");
         this.copy("_config.js", "gulp/config.js");
-        this.copy("_karma.conf.js", "karma.conf.js");
         this.copy(".gitignore", ".gitignore");
         this.copy("gulp/util/handleErrors.js", "gulp/util/handleErrors.js");
         this.copy("gulp/tasks/default.js", "gulp/tasks/default.js");
-        this.copy("gulp/tasks/jscs.js", "gulp/tasks/jscs.js");
-        this.copy("gulp/tasks/karma.js", "gulp/tasks/karma.js");
-        this.copy("gulp/tasks/less.js", "gulp/tasks/less.js");
-        this.copy("gulp/tasks/lint.js", "gulp/tasks/lint.js");
     },
 
     install: function () {
         this.npmInstall();
         this.npmInstall(['gulp'], { 'saveDev': true });
-        this.npmInstall(['less'], { 'saveDev': true });
-        this.npmInstall(['karma'], { 'saveDev': true });
-        this.npmInstall(['jscs'], { 'saveDev': true });
-        this.npmInstall(['eslint'], { 'saveDev': true });
+        if (this.includeLess) {
+            this.copy("gulp/tasks/less.js", "gulp/tasks/less.js");
+            this.npmInstall(['less'], { 'saveDev': true });
+        }
+        if (this.includeKarma) {
+            this.copy("gulp/tasks/karma.js", "gulp/tasks/karma.js");
+            this.copy("_karma.conf.js", "karma.conf.js");
+            this.npmInstall(['karma'], { 'saveDev': true });
+        }
+        if (this.includeJscs) {
+            this.copy("gulp/tasks/jscs.js", "gulp/tasks/jscs.js");
+            this.copy("_.jscsrc", ".jscsrc");
+            this.npmInstall(['jscs'], { 'saveDev': true });
+        }
+        if (this.includeEslint) {
+            this.copy("gulp/tasks/lint.js", "gulp/tasks/lint.js");
+            this.copy("_eslint.config.json", "eslint.config.json");
+            this.npmInstall(['eslint'], { 'saveDev': true });
+        }
     }
 });
