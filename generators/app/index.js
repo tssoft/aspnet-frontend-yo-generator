ef94d6101e2f71654879dce9450b9415597e94e7
s.lagutin <s.lagutin@ts-soft.ru>
2015-12-01 13:01:52 +0300

    gulp-react для ReactJS

--- generators/app/index.js
+++ generators/app/index.js
@@ -101,6 +101,8 @@ module.exports = yeoman.generators.Base.extend({
             }
             if (this.includeReact) {
                 this.bowerInstall(['react'], { 'save': true })
+                this.npmInstall(['react'], { 'saveDev': true });
+                this.copy("gulp/tasks/react.js", "gulp/tasks/react.js");
             }
             if (this.includeBackbone) {
                 this.bowerInstall(['backbone'], { 'save': true })
---
+++ generators/app/templates/gulp/tasks/react.js
@@ -0,0 +1,8 @@
+﻿var gulp = require('gulp');
+var react = require('gulp-react');
+
+gulp.task('react', function () {
+    return gulp.src('../../src/js/*.jsx')
+        .pipe(react())
+        .pipe(gulp.dest('dist'));
+});
