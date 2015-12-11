e9851ea3ba190cbb323dcb54112c7acdc605e921
s.lagutin <s.lagutin@ts-soft.ru>
2015-12-11 10:55:25 +0300

    Синхронный запуск конкатенаций

--- generators/app/templates/gulp/tasks/concatJs.js
+++ generators/app/templates/gulp/tasks/concatJs.js
@@ -1,7 +1,7 @@
 ﻿var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('concatjs', function () {
+gulp.task('concatjs', ['react'], function () {
     return gulp.src('build/scripts/**/*.js')
       .pipe(concat('app.js'))
       .pipe(gulp.dest('build/'));
