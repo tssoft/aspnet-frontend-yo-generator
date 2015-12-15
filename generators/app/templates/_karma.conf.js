0003e39f8b01ad6699328b0f67e5bc42a1588310
s.lagutin <s.lagutin@ts-soft.ru>
2015-12-15 11:26:13 +0300

    [363] Поддержка установленных фреймворков

--- generators/app/templates/_karma.conf.js
+++ generators/app/templates/_karma.conf.js
@@ -14,7 +14,11 @@ module.exports = function(config) {
 
 
         // list of files / patterns to load in the browser
-        files: [
+        files: [<% if (includeAngular) { %>
+            'bower_components/angular/angular.min.js',<% } if (includeReact) { %>
+            'bower_components/react/react.min.js',<% } if (includeBackbone) { %>
+            'bower_components/backbone/backbone-min.js',
+            'bower_components/underscore/underscore-min.js',<% } %>
             'build/scripts/**/*.js',
             'test/**/*.js'
         ],
