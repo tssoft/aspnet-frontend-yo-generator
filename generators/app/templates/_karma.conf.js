2958ef02ca9bd6c4e3e92feeef571d4e22a906ea
s.lagutin <s.lagutin@ts-soft.ru>
2015-12-17 15:28:34 +0300

    es5-shim in karma config

--- generators/app/templates/_karma.conf.js
+++ generators/app/templates/_karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine'<% if (includeReact) { %>, 'es5-shim'<% } %>],
 
 
         // list of files / patterns to load in the browser
