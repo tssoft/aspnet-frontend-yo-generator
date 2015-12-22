4699f5f1fa0b23aee714ad59f6719aa38abd65e4
s.lagutin <s.lagutin@ts-soft.ru>
2015-12-22 15:47:39 +0300

    test for Backbone

---
+++ test/backbone.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-generator').test;
+
+var rootTmp = path.join(__dirname, '.tmp');
+var gulpTasksPath = path.join(rootTmp, 'gulp/tasks');
+
+describe('backbone', function () {
+    before(function (done) {
+        helpers.run(path.join(__dirname, '../generators/app'))
+            .inDir(path.join(__dirname, '.tmp'))
+            .withPrompts({
+                features: ['includeBackbone'],
+                plugins: ['includeKarma'],
+                concatenatedSources: [],
+                reactPlugins: []
+            })
+            .on('end', done);
+    });
+
+    it('adds the file references to Karma', function () {
+        assert.fileContent(path.join(rootTmp, 'karma.conf.js'), 'backbone');
+        assert.fileContent(path.join(rootTmp, 'karma.conf.js'), 'underscore');
+    });
+});
