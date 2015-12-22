a9fa6b8e95cc5b5b1a94a849fd9e83a2fd879187
s.lagutin <s.lagutin@ts-soft.ru>
2015-12-22 15:30:27 +0300

    test for ESLint

---
+++ test/eslint.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-generator').test;
+
+var rootTmp = path.join(__dirname, '.tmp');
+var gulpTasksPath = path.join(rootTmp, 'gulp/tasks');
+
+describe('eslint', function () {
+    before(function (done) {
+        helpers.run(path.join(__dirname, '../generators/app'))
+            .inDir(path.join(__dirname, '.tmp'))
+            .withPrompts({
+                features: ['includeReact'],
+                plugins: ['includeEslint'],
+                concatenatedSources: [],
+                reactPlugins: []
+            })
+            .on('end', done);
+    });
+
+    it('marks as default task', function () {
+        assert.fileContent(path.join(gulpTasksPath, 'default.js'), 'lint');
+    });
+});
