84d731e633ff236559317c433c99c08d3b6079b1
s.lagutin <s.lagutin@ts-soft.ru>
2015-12-22 15:56:52 +0300

    test for concatJs

---
+++ test/concatJs.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-generator').test;
+
+var rootTmp = path.join(__dirname, '.tmp');
+var gulpTasksPath = path.join(rootTmp, 'gulp/tasks');
+
+describe('concatJs', function () {
+    before(function (done) {
+        helpers.run(path.join(__dirname, '../generators/app'))
+            .inDir(path.join(__dirname, '.tmp'))
+            .withPrompts({
+                features: ['includeReact'],
+                plugins: [],
+                concatenatedSources: ['includeConcatJs'],
+                reactPlugins: []
+            })
+            .on('end', done);
+    });
+
+    it('adds concatenation to react task', function () {
+        assert.fileContent(path.join(gulpTasksPath, 'react.js'), 'config.destConcat');
+    });
+});
