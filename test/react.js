e93c9d63c39b4a41bf37b1e1eacc401d147fe4e0
s.lagutin <s.lagutin@ts-soft.ru>
2015-12-22 15:19:13 +0300

    test for React

---
+++ test/react.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-generator').test;
+
+var rootTmp = path.join(__dirname, '.tmp');
+var gulpTasksPath = path.join(rootTmp, 'gulp/tasks');
+
+describe('react', function () {
+    before(function (done) {
+        helpers.run(path.join(__dirname, '../generators/app'))
+            .inDir(path.join(__dirname, '.tmp'))
+            .withPrompts({
+                features: ['includeReact'],
+                plugins: ['includeKarma'],
+                concatenatedSources: [],
+                reactPlugins: []
+            })
+            .on('end', done);
+    });
+
+    it('adds the file reference to Karma', function () {
+        assert.fileContent(path.join(rootTmp, 'karma.conf.js'), 'react');
+    });
+
+    it('creates all required files', function () {
+        assert.fileContent(path.join(gulpTasksPath, 'default.js'), 'react');
+    });
+});
